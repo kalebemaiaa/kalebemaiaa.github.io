1eef9ac8ee6f6a029e2353c623a42d2df00f758f
kalebemaia <kalebemaiaa@gmail.com>
2021-04-04 21:04:40 -0300

    termina site
    site definitivo feito

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*var botao=document.querySelector("#discord-button");
+
+
+function copyText(){
+    var discord="Kalebe#9951"
+    discord.select();
+    document.execCommand("Copy");
+    
+}
+
+botao.onclick=copyText
+
+https://www.linkedin.com/in/kalebe-felipe-04a0aa20b/
+*/
