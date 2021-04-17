c2037c01d0cea645991a9082d5418fc99460e0d5
kalebemaia <kalebemaiaa@gmail.com>
2021-04-16 23:14:52 -0300

    cria carrossel

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-var email=document.querySelector('#email')
-var assunto=document.querySelector('#assunto')
-
 function sendMail() {
+    var email=document.querySelector('#email')
+    var assunto=document.querySelector('#assunto')
     var link = "mailto:kalebe258456@gmail.com"
              + "?cc=" +encodeURIComponent(email.value)
              + "&subject=" + encodeURIComponent(assunto.value)
@@ -10,3 +9,23 @@ function sendMail() {
     
     window.location.href = link;
 }
+
+var btn_right=document.getElementById(`right-arrow`)
+var btn_left=document.getElementById(`left-arrow`)
+var carrossel=document.getElementById(`carrossel`)
+var pixels=50
+
+function mover_right(){
+    if(pixels<=1200){
+       pixels=pixels+50
+}
+    carrossel.style=`transform:translateX(${pixels}px)`
+}
+function mover_left(){
+    if(pixels>=-1200){
+        pixels=pixels-50
+}
+    carrossel.style=`transform:translateX(${pixels}px)`
+}
+btn_right.onclick=mover_right
+btn_left.onclick=mover_left
